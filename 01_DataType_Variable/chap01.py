dbf47573bfdb962e43692936f1f2e8eeddc31824
김은범 <icesnake72@gmail.com>
2024-01-20 01:03:39 +0900

    초기화 커밋

---
+++ 01_DataType_Variable/chap01.py
@@ -0,0 +1,18 @@
+'''
+기본 자료형의 종류와 출력
+'''
+
+name = '유재석' # 문자열
+age = 18    # 정수형
+height = 178.5  # 실수형
+address = '''서울시 강남구
+서초동 서초아파트 101동 101호'''    # 여러줄 문자열
+has_girlfriend = False  # 불린형
+job = None  # None
+
+print(name)
+print(age)
+print(height)
+print(address)
+print(has_girlfriend)
+print(job)
---
+++ 01_DataType_Variable/chap02.py
@@ -0,0 +1,10 @@
+'''
+자료형과 형변환
+'''
+
+height = 178.5
+print(height, type(height))
+
+# 실수 -> 정수
+ki = int(height)
+print(ki, type(ki))
