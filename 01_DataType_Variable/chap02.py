d57c8249930f572e5c4156d767e8168e85b6058d
icesnake72 <icesnake72@gmail.com>
2024-02-17 01:21:47 +0900

    형변환  & 연산자 추가

--- 01_DataType_Variable/chap02.py
+++ 01_DataType_Variable/chap02.py
@@ -8,3 +8,27 @@ print(height, type(height))
 # 실수 -> 정수
 ki = int(height)
 print(ki, type(ki))
+
+# 정수 -> 실수
+height = float(ki)
+print(height, type(height))
+
+# 문자열 -> 실수
+height = "178.5"
+height = float(height)
+print(height, type(height))
+
+# 실수 -> 문자열
+height = str(height)
+print(height, type(height))
+
+# 문자열 -> 정수
+ki = "178"
+ki = int(ki)
+print(ki, type(ki))
+
+# 정수 -> 문자열
+ki = str(ki)
+print(ki, type(ki))
+
+
--- 01_DataType_Variable/chap03.py
+++ 01_DataType_Variable/chap03.py
@@ -8,21 +8,48 @@ subtraction = num1 - num2
 multiplication = num1 * num2
 division = num1 / num2
 
-# 비교 연산 수행
-greater_than = num1 > num2
-less_than = num1 < num2
-equal_to = num1 == num2
-not_equal_to = num1 != num2
+# 몫과 나머지 연산 수행
+quotient = num1 // num2
+remainder = num1 % num2
 
 # 결과 출력
 print("덧셈 결과:", addition)
 print("뺄셈 결과:", subtraction)
 print("곱셈 결과:", multiplication)
 print("나눗셈 결과:", division)
+print("몫:", quotient)
+print("나머지:", remainder)
+
+# 비교 연산 수행
+greater_than = num1 > num2
+less_than = num1 < num2
+equal_to = num1 == num2
+not_equal_to = num1 != num2
+
 print("첫 번째 숫자가 두 번째 숫자보다 큰가?", greater_than)
 print("첫 번째 숫자가 두 번째 숫자보다 작은가?", less_than)
 print("두 숫자가 같은가?", equal_to)
 print("두 숫자가 다른가?", not_equal_to)
+
+# 대입 연산자의 종류와 활용
+num1 += num2
+print("+= 연산 후 결과:", num1)
+
+num1 -= num2
+print("-= 연산 후 결과:", num1)
+
+num1 *= num2
+print("*= 연산 후 결과:", num1)
+
+num1 /= num2
+print("/= 연산 후 결과:", num1)
+
+num1 //= num2
+print("//= 연산 후 결과:", num1)
+
+num1 %= num2
+print("%= 연산 후 결과:", num1)
+
 
 # 논리 연산 수행
 logical_and = num1 > 0 and num2 > 0
